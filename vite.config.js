14aac1ea21d3c7fb0e0998b2f8eb33c8f466b488
dsibi <dasibirin@gmail.com>
2023-06-05 13:51:43 +0300

    postcss config

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,12 @@ import { defineConfig } from "vite";
 import handlebars from "vite-plugin-handlebars";
 
 export default defineConfig({
-  plugins: [handlebars()],
+  plugins: [
+    handlebars({
+      // partialDirectory:,
+      context: {
+        username: "John Doe",
+      },
+    }),
+  ],
 });
